4241d092e7212b55350bce9a366e1716ef3ced40
Enes <enessimsek159@gmail.com>
2024-07-05 14:59:28 +0300

    Initial Commit

---
+++ ToDoZo/ToDoZoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoZoApp.swift
+//  ToDoZo
+//
+//  Created by Eno MacPro on 5.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoZoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
